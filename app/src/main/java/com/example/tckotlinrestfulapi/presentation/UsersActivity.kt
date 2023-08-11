ecab119a59dd8b90780259647e95f8594a672a05
Erick Savluchinske <ericksav.dev@gmail.com>
2023-08-11 17:37:08 -0300

    add recycler view

--- app/src/main/java/com/example/tckotlinrestfulapi/presentation/UsersActivity.kt
+++ app/src/main/java/com/example/tckotlinrestfulapi/presentation/UsersActivity.kt
@@ -2,10 +2,15 @@ package com.example.tckotlinrestfulapi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tckotlinrestfulapi.R
 import com.example.tckotlinrestfulapi.databinding.ActivityUsersBinding
+import com.example.tckotlinrestfulapi.presentation.user.UsersAdapter
 import com.example.tckotlinrestfulapi.presentation.user.UsersViewModel
 import com.example.tckotlinrestfulapi.presentation.user.UsersViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,10 +22,36 @@ class UsersActivity : AppCompatActivity() {
     lateinit var factory: UsersViewModelFactory
     private lateinit var usersViewModel: UsersViewModel
     private lateinit var binding: ActivityUsersBinding
+    private lateinit var adapter: UsersAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_users)
         usersViewModel = ViewModelProvider(this, factory)
             .get(UsersViewModel::class.java)
+
+        initRecyclerView()
     }
+
+    private fun initRecyclerView(){
+        binding.usersRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = UsersAdapter()
+        binding.usersRecyclerView.adapter = adapter
+        displayUsers()
+    }
+
+    private fun displayUsers(){
+        binding.usersProgressBar.visibility = View.VISIBLE
+        val responseLiveData = usersViewModel.getUsers()
+        responseLiveData.observe(this, Observer {
+            if (it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.usersProgressBar.visibility = View.GONE
+            }else{
+                binding.usersProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
 }
